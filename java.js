23d9e5257d96e73ee5337c66d6f10f428736b03d
Sardorbek <sobirjonovsardorbek842@gmail.com>
2022-10-25 12:13:54 +0500

    Initial commit

---
+++ java.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper(".banner-slide", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  var swiper = new Swiper(".slider2-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  
+  
+  
+  
+  
+  
